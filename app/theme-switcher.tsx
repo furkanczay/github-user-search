5b0f4d72ea1b0394078dac2e3982eb6a1d577681
Furkan Özay <furkanczay@gmail.com>
2025-06-08 11:07:09 +0300

    fix: theme-switcher

--- app/theme-switcher.tsx
+++ app/theme-switcher.tsx
@@ -1,23 +1,24 @@
 "use client";
 
-import {useTheme} from "next-themes";
+import { useTheme } from "next-themes";
 import { useEffect, useState } from "react";
 import { FaMoon, FaSun } from "react-icons/fa";
 
 export function ThemeSwitcher() {
-  const [mounted, setMounted] = useState(false)
-  const { theme, setTheme } = useTheme()
+  const [mounted, setMounted] = useState(false);
+  const { theme, setTheme } = useTheme();
 
   useEffect(() => {
-    setMounted(true)
-  }, [])
+    setMounted(true);
+  }, []);
 
-  if(!mounted) return null
+  if (!mounted) return null;
 
   return (
     <div>
-      
-      <button className="flex items-center gap-6" onClick={() => setTheme(theme === "dark" ? "light" : "dark")}>{theme === 'dark' ? (<>Light <FaSun /></>) : (<>Dark <FaMoon /></>)}</button>
+      <button onClick={() => setTheme(theme === "dark" ? "light" : "dark")}>
+        {theme === "dark" ? <FaSun /> : <FaMoon />}
+      </button>
     </div>
-  )
-};
+  );
+}
